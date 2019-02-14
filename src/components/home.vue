d2931e4a16f881282ebb7ab19a8d2ef5fbdb91ed
zhoujiacheng <zjc15039267308@163.com>
2019-02-14 12:07:32 +0800

    登录功能开发完毕

---
+++ src/components/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这是一个优秀的后台页面</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login'
+import Home from '@/components/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/login',
+      component: Login
+    },
+    { path: '/home', component: Home }
   ]
 })
